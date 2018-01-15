b60084ce98defc4492cbbe5644b5946271455e40
Morgan Stone <mstone@designbymorgan.com>
2018-01-15 08:35:15 -0600

    add production key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBfYC-7Req9TolEhMsbgIHS2fKqihx3nSU",
+    authDomain: "soap-448f4.firebaseapp.com",
+    databaseURL: "https://soap-448f4.firebaseio.com",
+    projectId: "soap-448f4",
+    storageBucket: "soap-448f4.appspot.com",
+    messagingSenderId: "671604258622"
+  }
 };
